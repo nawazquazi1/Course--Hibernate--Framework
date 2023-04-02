ee0d23ea835eafccbc159a6fc87b7eb8c14d877d
nawaz quazi <nawazquazi356@gmail.com>
2023-04-02 15:51:45 +0530

    Coures--Hibernate

---
+++ src/main/java/org/hiibernate/Insert.java
@@ -0,0 +1,32 @@
+package org.hiibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Insert {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Users.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			// Create object of entity class type
+			Users user = new Users("username", "password", "firstName", "lastName");
+			// Start transaction
+			session.beginTransaction();
+			// Perform operation
+			session.save(user);
+			// Commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Row added!");
+
+		} finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+}
